d7bb9e682d5ca66542ac54d8e39ba3c5aeec6d8f
ScottSerpa <104799263+ScottSerpa@users.noreply.github.com>
2023-03-10 21:53:41 -0800

    Update combine_pos_neg_categorize.py

--- combine_pos_neg_categorize.py
+++ combine_pos_neg_categorize.py
@@ -20,7 +20,7 @@ neg = pd.read_csv('comments_negative.csv')
 pos = pd.read_csv('comments_positive.csv')
 
 
-# In[54]:
+# In[178]:
 
 
 frames = [pos, neg]
@@ -28,42 +28,71 @@ frames = [pos, neg]
 final = pd.concat(frames)
 
 
-# In[56]:
+# In[179]:
 
 
-final = final.sample(n=100000)
+final# = final.sample(n=5000000)
 
 
-# In[60]:
+# In[182]:
 
 
-def scale_number(unscaled, to_min, to_max, from_min, from_max):
-    return round((to_max-to_min)*(unscaled-from_min)/(from_max-from_min)+to_min)
+#def scale_list(l, to_min, to_max):
+#    arr = np.array(l)
+#    scaled_arr = (arr - arr.min()) * (to_max - to_min) / (arr.max() - arr.min()) + to_min
+#    return list(np.round(scaled_arr).astype(int))
 
-def scale_list(l, to_min, to_max):
-    return [scale_number(i, to_min, to_max, min(l), max(l)) for i in l]
-
-
-# In[61]:
+final['score_scaled'] = pd.qcut(final.score,[0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1], labels=['1','2','3','4','5','6','7','8','9','10'])
 
 
-final["score_scaled"] = scale_list(final["score"], 1, 10)
+# In[184]:
 
 
-# In[70]:
+final.groupby(['score_scaled'])['text'].count()
 
 
-final_dropped = final.drop(columns=['id', 'parent_id', 'parent_link_id', 'subreddit_id', 'link_id', 'score', 'ups', 'author', 'controversiality', 'parent_ups', 'parent_author'])
+# In[185]:
 
 
-# In[78]:
+#final["score_scaled"] = scale_list(final["score"], 1, 10)
+
+#final["score_scaled"] = [scale_list(final["score"], 1, 10) for score in final["score"]]
 
 
-sampled = final_dropped.sample(n=60000)
+# In[186]:
 
 
-# In[79]:
+#PICK UP FROM HERE IF MISTAKE
+
+final_dropped = final.drop(columns=['id', 'parent_id', 'parent_link_id', 'subreddit_id', 'link_id', 'score', 'ups', 'author', 'controversiality', 'parent_ups', 'parent_author', 'parent_text', 'parent_score', 'parent_controversiality'])
+
+
+# In[187]:
+
+
+sampled = final_dropped.sample(n=60000, random_state = 5422)
+
+
+# In[188]:
+
+
+sampled['text'] = sampled['text'].str.replace('\W', ' ', regex=True)
+
+
+# In[190]:
+
+
+sampled.groupby(['score_scaled'])['text'].count()
+
+
+# In[192]:
 
 
 sampled.to_csv("reddit_comments.csv", index=False)
+
+
+# In[ ]:
+
+
+
 
